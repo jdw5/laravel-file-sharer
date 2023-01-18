eeffb0135b9f0d2c8ede5638046aa1d74ace4b6f
jdw5 <joshuadwallace1@gmail.com>
2023-01-18 15:51:30 +1000

    AppLayout initial file

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="w-full md:w-8/12 mx-auto lg:w-6/12 md:mt-20 mt-6 px-6">
+        <header class="flex -mx-3 -mt-3 flex-wrap md:flex-nowrap items-center justify-between mb-6">
+            <ul class="flex items-center">
+                <li>
+                    <a href="" class="text-sm inline-block p-3 text-gray-800">
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a href="" class="text-sm inline-block p-3 text-gray-800">
+                        Your Files
+                    </a>
+                </li>
+            </ul>
+            <ul class="flex items-center">
+                <li>
+                    <a href="" class="text-sm inline-block p-3 text-gray-800">
+                        Sig in
+                    </a>
+                </li>
+                <li>
+                    <a href="" class="text-sm inline-block p-3 text-gray-800">
+                        Create account
+                    </a>
+                </li>
+
+            </ul>
+        </header>
+        <main class="">
+            <slot />
+        </main>
+    </div>
+</template>
+
+<script>
+</script>
