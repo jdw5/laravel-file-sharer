9ea55044bd7ecca0eae470b96d949e075e674db9
jdw5 <joshuadwallace1@gmail.com>
2023-02-04 12:48:38 +1000

    Plan seeder data

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -2,12 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Plan extends Model
 {
     use HasFactory;
+
+    public static function booted()
+    {
+        static::creating(function (Plan $plan) {
+            $plan->slug = Str::slug($plan->name);
+        });
+    }
 
     public static function free()
     {
--- database/seeders/PlanSeeder.php
+++ database/seeders/PlanSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Plan;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,31 @@ class PlanSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Plan::create([
+            'name' => 'Free',
+            'price' => 0,
+            'purchasable' => false,
+            'storage' => 5242880,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        Plan::create([
+            'name' => 'Base',
+            'price' => 400,
+            'purchasable' => true,
+            'storage' => 10485760,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        Plan::create([
+            'name' => 'Large',
+            'price' => 800,
+            'purchasable' => true,
+            'storage' => 15728640,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
     }
 }
